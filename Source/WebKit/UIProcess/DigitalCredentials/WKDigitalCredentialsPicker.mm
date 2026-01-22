clopez/WebKit

commit 3d2027c319fe6dc192340ebbe62d239f0e9d4b70

Digital credentials: don't do any processing if there's no UI available rdar://167807196 https://bugs.webkit.org/show_bug.cgi?id=305158
Reviewed by Pascoe.

We now check whether digital credentials UI is available at the platform level
before attempting to process any digital credentials related requests.

This provides better developer ergonomics by allowing the API to behave
the same way as it does on legacy platforms that don't support the API.

Related spec change:
https://github.com/w3c-fedid/digital-credentials/pull/395

Relies on existing tests.

* Source/WebCore/Modules/credentialmanagement/CredentialsContainer.cpp:
(WebCore::CredentialsContainer::get):
* Source/WebCore/Modules/identity/CredentialRequestCoordinator.h:
* Source/WebCore/Modules/identity/DigitalCredential.cpp:
(WebCore::DigitalCredential::discoverFromExternalSource):
* Source/WebCore/loader/EmptyClients.cpp:
(WebCore::pageConfigurationWithEmptyClients):
* Source/WebCore/page/Chrome.cpp:
* Source/WebCore/page/Chrome.h:
* Source/WebCore/page/ChromeClient.cpp:
* Source/WebCore/page/ChromeClient.h:
* Source/WebCore/page/Page.cpp:
* Source/WebCore/page/Page.h:
(WebCore::Page::authenticatorCoordinator):
* Source/WebCore/page/PageConfiguration.cpp:
(WebCore::PageConfiguration::PageConfiguration):
* Source/WebCore/page/PageConfiguration.h:
* Source/WebKit/Shared/WebCoreArgumentCoders.serialization.in:
* Source/WebKit/UIProcess/API/Cocoa/WKWebView.mm:
* Source/WebKit/UIProcess/API/Cocoa/WKWebViewInternal.h:
* Source/WebKit/UIProcess/DigitalCredentials/WKDigitalCredentialsPicker.h:
* Source/WebKit/UIProcess/DigitalCredentials/WKDigitalCredentialsPicker.mm:
* Source/WebKit/UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::showDigitalCredentialsPicker):
(WebKit::WebPageProxy::fetchRawDigitalCredentialRequests):
(WebKit::WebPageProxy::dismissDigitalCredentialsPicker):
* Source/WebKit/UIProcess/WebPageProxy.messages.in:
* Source/WebKit/UIProcess/ios/PageClientImplIOS.h:

Source/WebKit/UIProcess/DigitalCredentials/WKDigitalCredentialsPicker.mm
@@ -26,7 +26,7 @@
 #import "config.h"
 #import "WKDigitalCredentialsPicker.h"
 
-#if HAVE(DIGITAL_CREDENTIALS_UI)
+#if ENABLE(WEB_AUTHN)
 
 #if PLATFORM(IOS_FAMILY)
 #import "UIKitSPI.h"
@@ -448,4 +448,4 @@ static RetainPtr<NSArray<NSArray<WKIdentityDocumentPresentmentRequestAuthenticat
 
 @end // WKDigitalCredentialsPicker
 
-#endif // HAVE(DIGITAL_CREDENTIALS_UI)
+#endif // ENABLE(WEB_AUTHN)

Source/WebCore/Modules/credentialmanagement/CredentialsContainer.cpp
@@ -55,10 +55,13 @@ void CredentialsContainer::get(CredentialRequestOptions&& options, CredentialPro
 
 #if ENABLE(WEB_AUTHN)
     if (options.digital) {
+#if HAVE(DIGITAL_CREDENTIALS_UI)
         DigitalCredential::discoverFromExternalSource(*document(), WTF::move(promise), WTF::move(options));
+#else
+        promise.resolve(nullptr);
+#endif
         return;
     }
-
     document()->page()->authenticatorCoordinator().discoverFromExternalSource(*document(), WTF::move(options), WTF::move(promise));
 #else
     promise.resolve(nullptr);

Source/WebCore/Modules/identity/CredentialRequestCoordinator.h
@@ -68,14 +68,6 @@ public:
 private:
     void dismissPickerAndSettle(ExceptionOr<RefPtr<BasicCredential>>&&);
 
-    static constexpr bool canPresentDigitalCredentialsUI()
-    {
-#if HAVE(DIGITAL_CREDENTIALS_UI)
-        return true;
-#else
-        return false;
-#endif
-    }
     class PickerStateGuard final {
     public:
         explicit PickerStateGuard(CredentialRequestCoordinator&);

Source/WebCore/Modules/identity/DigitalCredential.cpp
@@ -173,12 +173,8 @@ void DigitalCredential::discoverFromExternalSource(const Document& document, Cre
         return;
     }
 
-#if HAVE(DIGITAL_CREDENTIALS_UI)
     Ref coordinator = page->credentialRequestCoordinator();
     coordinator->prepareCredentialRequest(document, WTF::move(promise), presentationRequestsOrException.releaseReturnValue(), options.signal);
-#else
-    promise.reject(Exception { ExceptionCode::NotSupportedError, "Digital credentials are not supported."_s });
-#endif
 }
 
 } // namespace WebCore

Source/WebCore/loader/EmptyClients.cpp
@@ -103,7 +103,7 @@
 #include "LegacyPreviewLoaderClient.h"
 #endif
 
-#if HAVE(DIGITAL_CREDENTIALS_UI)
+#if ENABLE(WEB_AUTHN)
 #include "DigitalCredentialsRequestData.h"
 #include "DigitalCredentialsResponseData.h"
 #include "ExceptionData.h"
@@ -485,7 +485,7 @@ WTF_MAKE_TZONE_ALLOCATED_IMPL(EmptyPaymentCoordinatorClient);
 
 #endif
 
-#if HAVE(DIGITAL_CREDENTIALS_UI)
+#if ENABLE(WEB_AUTHN)
 class EmptyCredentialRequestCoordinatorClient final : public CredentialRequestCoordinatorClient {
     WTF_MAKE_TZONE_ALLOCATED(EmptyCredentialRequestCoordinatorClient);
 public:
@@ -1275,7 +1275,7 @@ PageConfiguration pageConfigurationWithEmptyClients(std::optional<PageIdentifier
         makeUniqueRef<EmptyChromeClient>(),
         makeUniqueRef<EmptyCryptoClient>(),
         makeUniqueRef<DocumentSyncClient>()
-#if HAVE(DIGITAL_CREDENTIALS_UI)
+#if ENABLE(WEB_AUTHN)
         , EmptyCredentialRequestCoordinatorClient::create()
 #endif
     };

Source/WebCore/page/Chrome.cpp
@@ -31,10 +31,8 @@
 #include "ContainerNodeInlines.h"
 #include "DataListSuggestionPicker.h"
 #include "DateTimeChooser.h"
-#if HAVE(DIGITAL_CREDENTIALS_UI)
 #include "DigitalCredentialsRequestData.h"
 #include "DigitalCredentialsResponseData.h"
-#endif
 #include "DocumentType.h"
 #include "DocumentView.h"
 #include "DocumentWindow.h"
@@ -478,7 +476,7 @@ void Chrome::showContactPicker(ContactsRequestData&& requestData, CompletionHand
     m_client->showContactPicker(WTF::move(requestData), WTF::move(callback));
 }
 
-#if HAVE(DIGITAL_CREDENTIALS_UI)
+#if ENABLE(WEB_AUTHN)
 void Chrome::showDigitalCredentialsPicker(const DigitalCredentialsRequestData& requestData, WTF::CompletionHandler<void(Expected<WebCore::DigitalCredentialsResponseData, WebCore::ExceptionData>&&)>&& callback)
 {
     m_client->showDigitalCredentialsPicker(requestData, WTF::move(callback));

Source/WebCore/page/Chrome.h
@@ -93,7 +93,7 @@ struct ShareDataWithParsedURL;
 struct ViewportArguments;
 struct WindowFeatures;
 
-#if HAVE(DIGITAL_CREDENTIALS_UI)
+#if ENABLE(WEB_AUTHN)
 class SecurityOriginData;
 
 struct DigitalCredentialsRequestData;
@@ -212,7 +212,7 @@ public:
     void showShareSheet(ShareDataWithParsedURL&&, CompletionHandler<void(bool)>&&);
     void showContactPicker(ContactsRequestData&&, CompletionHandler<void(std::optional<Vector<ContactInfo>>&&)>&&);
 
-#if HAVE(DIGITAL_CREDENTIALS_UI)
+#if ENABLE(WEB_AUTHN)
     void showDigitalCredentialsPicker(const DigitalCredentialsRequestData&, WTF::CompletionHandler<void(Expected<WebCore::DigitalCredentialsResponseData, WebCore::ExceptionData>&&)>&&);
     void dismissDigitalCredentialsPicker(CompletionHandler<void(bool)>&&);
 #endif

Source/WebCore/page/ChromeClient.cpp
@@ -97,7 +97,7 @@ RefPtr<ShapeDetection::TextDetector> ChromeClient::createTextDetector() const
     return nullptr;
 }
 
-#if HAVE(DIGITAL_CREDENTIALS_UI)
+#if ENABLE(WEB_AUTHN)
 ExceptionOr<Vector<ValidatedDigitalCredentialRequest>> ChromeClient::validateAndParseDigitalCredentialRequests(const SecurityOrigin&, const Document&, const Vector<UnvalidatedDigitalCredentialRequest>&)
 {
     return Exception { ExceptionCode::NotSupportedError, "Digital credentials are not supported."_s };

commit 08f200fa7654f924f7afc8f00f249f1be45c474f

Build broken on MacOS 14 / Xcode 15.4 / Clang 15 after 305889@main https://bugs.webkit.org/show_bug.cgi?id=306015
Reviewed by NOBODY (OOPS!).

Use uniform initialization (braces) for DigitalCredentialsResponseData.
Xcode 15.4 (Apple Clang 15) does not support C++20 parenthesized
initialization for aggregates (P0960R3), which was introduced in
Clang 16. Braces ensure compatibility with older toolchains.

This started to fail after 305889@main because this code was previously
not compiled on MacOS 14 (guard DIGITAL_CREDENTIALS_UI) but now is
(guard WEB_AUTHN).

* Source/WebKit/UIProcess/DigitalCredentials/WKDigitalCredentialsPicker.mm:
(-[WKDigitalCredentialsPicker handlePresentmentCompletionWithResponse:error:]):

Source/WebKit/UIProcess/DigitalCredentials/WKDigitalCredentialsPicker.mm
@@ -372,7 +372,7 @@ static RetainPtr<NSArray<NSArray<WKIdentityDocumentPresentmentRequestAuthenticat
         if ([protocol isEqualToString:@"org.iso.mdoc"]) {
             Ref object = JSON::Object::create();
             object->setString("response"_s, responseData);
-            auto responseObject = WebCore::DigitalCredentialsResponseData(IdentityCredentialProtocol::OrgIsoMdoc, object->toJSONString());
+            auto responseObject = WebCore::DigitalCredentialsResponseData { IdentityCredentialProtocol::OrgIsoMdoc, object->toJSONString() };
             [self completeWith:WTF::move(responseObject)];
         } else {
             LOG(DigitalCredentials, "Unknown protocol response from document provider. Can't convert it %s.", [protocol UTF8String]);