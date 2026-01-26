bb6e6b3d27e657c98ce49a44a4748fb91508f3ec
Carlos Alberto Lopez Perez <clopez@igalia.com>
2026-01-26 21:42:30 +0000

    Revert "Re-land [Swift in WebKit] Remove a few module compilation guards from a few more places (again)"
    This reverts commit 90d660be3d3170d52d3eaea01e0d3d0e006c2245.

--- Source/WebKit/UIProcess/mac/WebViewImpl.h
+++ Source/WebKit/UIProcess/mac/WebViewImpl.h
@@ -25,8 +25,6 @@
 
 #pragma once
 
-#if !__has_feature(modules) || (defined(WK_SUPPORTS_SWIFT_OBJCXX_INTEROP) && WK_SUPPORTS_SWIFT_OBJCXX_INTEROP)
-
 #include <wtf/Platform.h>
 
 #if PLATFORM(MAC)
@@ -1134,5 +1132,3 @@ ALLOW_DEPRECATED_DECLARATIONS_END
 } // namespace WebKit
 
 #endif // PLATFORM(MAC)
-
-#endif // !__has_feature(modules) || (defined(WK_SUPPORTS_SWIFT_OBJCXX_INTEROP) && WK_SUPPORTS_SWIFT_OBJCXX_INTEROP)
