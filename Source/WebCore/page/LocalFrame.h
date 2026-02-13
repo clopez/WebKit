f78fbaa50e568acedca7b108188b3146df53bc0d
Carlos Alberto Lopez Perez <clopez@igalia.com>
2026-02-13 13:48:27 +0100

    Fix build: Drop Playwright patch over LocalFrame.{h,ccp} and recreate it.
    The current Playwright patch over LocalFrame.{h,ccp} adds some
    functions that were copied from Source/WebCore/page/ios/FrameIOS.mm
    and Source/WebKit/WebProcess/WebPage/ios/WebPageIOS.mm
    
    This is not needed anymore since 307290@main commit upstream, which
    has exposed the neded functions in LocalFrame.{h,ccp} to all
    Apple platforms (COCOA).
    
    So that caused build errors due to duplicated functions like
    LocalFrame::hitTestResultAtViewportLocation.
    
    The functions that we were copying looked like old copies of the
    current ones without any major significant change.
    So to reduce the delta of the patch, and to also use the newer
    functions it was decided to drop this part of the patch and adapt
    the current LocalFrame.{h,ccp}, which only needed to incorporate
    a few relevant changes from the original patch and to expose to
    non-cocoa platforms the relevant functions that 307290@main
    moved from the iOS FrameIOS.mm and WebPageIOS.mm to LocalFrame.{h,ccp}.
    
    This functions are needed to be exposed to other ports to be able
    to call frame->hasScreenSizeOverride() at Source/WebCore/page/Screen.cpp
    and function localMainFrame->nodeRespondingToClickEvents at file
    Source/WebKit/WebProcess/WebPage/WebPage.cpp.
    
    Commit causing the conflicts:
      241ce0e70ab7 [AppKit Gestures] Expose `WebPage::requestPositionInformation` on macOS
      https://commits.webkit.org/307290@main

--- Source/WebCore/page/LocalFrame.h
+++ Source/WebCore/page/LocalFrame.h
@@ -130,9 +130,7 @@ enum {
 enum OverflowScrollAction { DoNotPerformOverflowScroll, PerformOverflowScroll };
 #endif
 
-#if PLATFORM(COCOA)
-using NodeQualifier = Function<Node* (const HitTestResult&, Node* terminationNode, IntRect* nodeBounds)>;
-#endif
+using NodeQualifier = Function<RefPtr<Node> (const HitTestResult&, Node* terminationNode, IntRect* nodeBounds)>;
 
 class LocalFrame final : public Frame {
 public:
@@ -236,7 +234,6 @@ public:
     WEBCORE_EXPORT DataDetectionResultsStorage& dataDetectionResults();
 #endif
 
-#if PLATFORM(COCOA)
     RefPtr<Node> betterApproximateNode(const IntPoint& testPoint, const NodeQualifier&, Node* best, Node* failedNode, IntPoint& bestPoint, IntRect& bestRect, const IntRect& testRect);
 
     WEBCORE_EXPORT RefPtr<Node> nodeRespondingToInteraction(const FloatPoint& viewportLocation, FloatPoint& adjustedViewportLocation);
@@ -250,15 +247,14 @@ public:
     WEBCORE_EXPORT RefPtr<Node> nodeRespondingToDoubleClickEvent(const FloatPoint& viewportLocation, FloatPoint& adjustedViewportLocation);
 
     static bool nodeWillRespondToMouseEvents(Node&);
-#endif // PLATFORM(COCOA)
-
-    WEBCORE_EXPORT Node* deepestNodeAtLocation(const FloatPoint& viewportLocation);
-    WEBCORE_EXPORT RefPtr<Node> nodeRespondingToScrollWheelEvents(const FloatPoint& viewportLocation);
-    WEBCORE_EXPORT RefPtr<Node> approximateNodeAtViewportLocationLegacy(const FloatPoint& viewportLocation, FloatPoint& adjustedViewportLocation);
 
 #if PLATFORM(IOS_FAMILY)
     const ViewportArguments& viewportArguments() const;
     WEBCORE_EXPORT void setViewportArguments(const ViewportArguments&);
+
+    WEBCORE_EXPORT Node* deepestNodeAtLocation(const FloatPoint& viewportLocation);
+    WEBCORE_EXPORT RefPtr<Node> nodeRespondingToScrollWheelEvents(const FloatPoint& viewportLocation);
+    WEBCORE_EXPORT RefPtr<Node> approximateNodeAtViewportLocationLegacy(const FloatPoint& viewportLocation, FloatPoint& adjustedViewportLocation);
 
     WEBCORE_EXPORT NSArray *wordsInCurrentParagraph() const;
     WEBCORE_EXPORT CGRect renderRectForPoint(CGPoint, bool* isReplaced, float* fontSize) const;
