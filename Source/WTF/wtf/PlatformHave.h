1a52d087fce9f44a4ad30358c8dccfbdc1f3889d
Carlos Alberto Lopez Perez <clopez@igalia.com>
2026-03-13 10:46:33 +0000

    Resolve merge conflicts after 309154@main
    HAVE_GPU_AVAILABILITY_CHECK is gone, the check is unconditionally not longer done,
    which fortunately matches with the current PW selection (not doing the check), so
    the patch can just be dropped.
    
    Commit causing the merge conflicts:
      887865867066 WebGL: Do not support in-process WebGL in Workers
      https://commits.webkit.org/309154@main

--- Source/WTF/wtf/PlatformHave.h
+++ Source/WTF/wtf/PlatformHave.h
@@ -1063,19 +1063,6 @@
 #define HAVE_POWERLOG_TASK_MODE_QUERY 1
 #endif
 
-<<<<<<< HEAD
-||||||| parent of 18883ec2b696 (chore(webkit): bootstrap build #2269)
-#if PLATFORM(MAC)
-#define HAVE_GPU_AVAILABILITY_CHECK 1
-#endif
-
-=======
-#if PLATFORM(MAC)
-// Playwright: disable the check to make WebGL always work.
-#define HAVE_GPU_AVAILABILITY_CHECK 0
-#endif
-
->>>>>>> 18883ec2b696 (chore(webkit): bootstrap build #2269)
 #if !defined(HAVE_LOCKDOWN_MODE_PDF_ADDITIONS) && \
     (PLATFORM(MAC) || (PLATFORM(IOS) && !PLATFORM(IOS_SIMULATOR)))
 #define HAVE_LOCKDOWN_MODE_PDF_ADDITIONS 1
