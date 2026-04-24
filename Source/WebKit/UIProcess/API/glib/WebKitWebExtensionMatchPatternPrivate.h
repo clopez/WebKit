9ff423e8e40bf33656cdc2e0211ecb826846da92
Carlos Alberto Lopez Perez <clopez@igalia.com>
2026-04-24 20:52:14 +0000

    fix_build_for_build-rpi4-like-buildroot.sh

--- Source/WebKit/UIProcess/API/glib/WebKitWebExtensionMatchPatternPrivate.h
+++ Source/WebKit/UIProcess/API/glib/WebKitWebExtensionMatchPatternPrivate.h
@@ -20,7 +20,7 @@
 #pragma once
 
 #include "WebExtensionMatchPattern.h"
-#include "WebKitWebExtensionMatchPattern.h"
+#include "WebKitWebExtensionMatchPatternPrivate.h"
 #include <wtf/RefPtr.h>
 
 typedef struct _WebKitWebExtensionMatchPattern WebKitWebExtensionMatchPattern;
