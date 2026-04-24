ca72f218f6b0aa2177bb86a58867930bcb02ff26
Carlos Alberto Lopez Perez <clopez@igalia.com>
2026-04-24 18:49:32 +0000

    Drop the PlayWright changes from AcceleratedSurface.h after 311656@main
    That is not longer needed as the build fix is now usptream.
    
    Commit that integrated this fix upstream:
      dc1afde2e8b9 REGRESSION(306842@main): [WPE] Build with ENABLE_WPE_PLATFORM=OFF fails
      https://commits.webkit.org/311656@main

--- Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/AcceleratedSurface.h
+++ Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/AcceleratedSurface.h
@@ -117,7 +117,7 @@ public:
 #endif
     }
 
-#if PLATFORM(GTK) || PLATFORM(WPE) 
+#if PLATFORM(GTK) || ENABLE(WPE_PLATFORM)
     bool usesGL() const { return m_renderingPurpose == RenderingPurpose::Composited || m_hardwareAccelerationEnabled; }
 #elif PLATFORM(WPE)
     constexpr bool usesGL() const { return true; }
@@ -341,7 +341,7 @@ private:
 
         enum class Type {
             Invalid,
-#if PLATFORM(GTK) || PLATFORM(WPE)
+#if PLATFORM(GTK) || ENABLE(WPE_PLATFORM)
 #if USE(GBM) || OS(ANDROID)
             EGLImage,
 #endif
