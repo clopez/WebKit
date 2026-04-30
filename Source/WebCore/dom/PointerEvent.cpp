8269bb1031910c4017f90e5d489e896338379a2b
Carlos Alberto Lopez Perez <clopez@igalia.com>
2026-04-30 16:55:32 +0000

    Fix MacOS build failure after 312332@main
    Commit causing the build failure:
      23248aa32f59 Add GenericTimeMixin::approximate() template function.
      https://commits.webkit.org/312332@main

--- Source/WebCore/dom/PointerEvent.cpp
+++ Source/WebCore/dom/PointerEvent.cpp
@@ -422,7 +422,7 @@ Ref<PointerEvent> PointerEvent::create(const AtomString& type, const PlatformTou
 }
 
 PointerEvent::PointerEvent(const AtomString& type, const PlatformTouchEvent& event, const Vector<Ref<PointerEvent>>& coalescedEvents, const Vector<Ref<PointerEvent>>& predictedEvents, CanBubble canBubble, IsCancelable isCancelable, unsigned touchIndex, bool isPrimary, Ref<WindowProxy>&& view, const DoublePoint& touchDelta)
-    : MouseEvent(EventInterfaceType::PointerEvent, type, canBubble, isCancelable, typeIsComposed(type), event.timestamp().approximateMonotonicTime(), WTF::move(view), 0,
+    : MouseEvent(EventInterfaceType::PointerEvent, type, canBubble, isCancelable, typeIsComposed(type), event.timestamp().approximate<MonotonicTime>(), WTF::move(view), 0,
         event.touchPoints().at(touchIndex).pos(), event.touchPoints().at(touchIndex).pos(), touchDelta.x(), touchDelta.y(), event.modifiers(), buttonForType(type), buttonsForType(type), nullptr, 0, SyntheticClickType::NoTap, { }, { }, std::nullopt, IsSimulated::No, IsTrusted::Yes)
     , m_pointerId(event.touchPoints().at(touchIndex).id())
     , m_width(2 * event.touchPoints().at(touchIndex).radius().width())
